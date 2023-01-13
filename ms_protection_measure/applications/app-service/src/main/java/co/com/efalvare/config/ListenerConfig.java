cc02f24a6852260198989b949cbcbb323c1c3d23
Edwin Felipe Alvarez Rincon <efalvare@bancolombia.com>
2023-01-12 21:03:36 -0500

    [Adiciona] Microservicio de protection measure

---
+++ ms_protection_measure/applications/app-service/src/main/java/co/com/efalvare/config/ListenerConfig.java
@@ -0,0 +1,25 @@
+package co.com.efalvare.config;
+
+import co.com.efalvare.mq.listener.createpm.CreatePMRabbitHandler;
+import commons.rabbit.createpm.query.CreatePMQuery;
+import lombok.RequiredArgsConstructor;
+import org.reactivecommons.async.api.HandlerRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static commons.constants.RabbitOperations.CREATE_PROTECTION_MEASURE;
+
+@Configuration
+@RequiredArgsConstructor
+public class ListenerConfig {
+
+
+    private final CreatePMRabbitHandler createPMRabbitHandler;
+
+    @Bean
+    public HandlerRegistry handlerRegistry(){
+        return HandlerRegistry.register()
+                .serveQuery(CREATE_PROTECTION_MEASURE, createPMRabbitHandler::createPM, CreatePMQuery.class);
+    }
+
+}
