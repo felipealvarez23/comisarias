875916fbc6ab4b0bb858f1671e64976c025179d6
Edwin Felipe Alvarez Rincon <efalvare@bancolombia.com>
2023-01-12 21:07:16 -0500

    [Remueve] Comensatior en el codigo

--- ms_protection_measure/infrastructure/entry-points/mq-listener/src/main/java/co/com/efalvare/mq/listener/createpm/CreatePMRabbitHandler.java
+++ ms_protection_measure/infrastructure/entry-points/mq-listener/src/main/java/co/com/efalvare/mq/listener/createpm/CreatePMRabbitHandler.java
@@ -1,10 +1,5 @@
 package co.com.efalvare.mq.listener.createpm;
 
-/*import co.com.efalvare.model.protectionmeasure.ProtectionMeasure;
-import co.com.efalvare.model.rabbit.createpm.query.CreatePMDataQuery;
-import co.com.efalvare.model.rabbit.createpm.query.CreatePMQuery;
-import co.com.efalvare.model.rabbit.createpm.reply.CreatePMDataReply;
-import co.com.efalvare.model.rabbit.createpm.reply.CreatePMReply;*/
 import co.com.efalvare.usecase.protectionmeasure.ProtectionMeasureUseCase;
 import commons.rabbit.createpm.query.CreatePMQuery;
 import commons.rabbit.createpm.reply.CreatePMReply;
@@ -21,22 +16,5 @@ public class CreatePMRabbitHandler {
     public Mono<CreatePMReply> createPM(CreatePMQuery query) {
         return Mono.empty();
     }
-
-
-/*    public Mono<CreatePMReply> createPM(CreatePMQuery query) {
-        CreatePMDataQuery data = query.getData();
-        return useCase.createProtectionMeasure(ProtectionMeasure.builder()
-                .documentType(data.getDocumentType())
-                .documentNumber(data.getDocumentNumber())
-                .type(data.getType())
-                .ip("10.12.23.15")
-                .build())
-                .map(protectionMeasure -> CreatePMReply.builder()
-                        .data(CreatePMDataReply.builder()
-                                .protectionMeasureId(protectionMeasure.getRequestId())
-                                .type(protectionMeasure.getType())
-                                .build())
-                        .build());
-    }*/
 
 }
