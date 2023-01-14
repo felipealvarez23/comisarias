b74dbaf0471bebe0b69b35e1e0837507671bac88
Edwin Felipe Alvarez Rincon <efalvare@bancolombia.com>
2023-01-14 11:28:02 -0500

    [Adiciona] Logs a la operacion rabbit createPM

--- ms_protection_measure/infrastructure/entry-points/mq-listener/src/main/java/co/com/efalvare/mq/listener/createpm/CreatePMRabbitHandler.java
+++ ms_protection_measure/infrastructure/entry-points/mq-listener/src/main/java/co/com/efalvare/mq/listener/createpm/CreatePMRabbitHandler.java
@@ -4,9 +4,11 @@ import co.com.efalvare.usecase.protectionmeasure.ProtectionMeasureUseCase;
 import commons.rabbit.createpm.query.CreatePMQuery;
 import commons.rabbit.createpm.reply.CreatePMReply;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class CreatePMRabbitHandler {
@@ -14,6 +16,7 @@ public class CreatePMRabbitHandler {
     private final ProtectionMeasureUseCase useCase;
 
     public Mono<CreatePMReply> createPM(CreatePMQuery query) {
+        log.info("Rabbit request operation [create-protection_measure] received");
         return Mono.empty();
     }
 
