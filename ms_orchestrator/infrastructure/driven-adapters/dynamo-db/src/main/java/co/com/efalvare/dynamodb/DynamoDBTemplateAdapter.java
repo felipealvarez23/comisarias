4573a9c8c9f99da4c4cf5e3e9e0543183354e90b
Edwin Felipe Alvarez Rincon <efalvare@bancolombia.com>
2023-03-18 11:53:55 -0500

    [Ajusta] Pipeline lib_commons_comisarias

--- ms_orchestrator/infrastructure/driven-adapters/dynamo-db/src/main/java/co/com/efalvare/dynamodb/DynamoDBTemplateAdapter.java
+++ ms_orchestrator/infrastructure/driven-adapters/dynamo-db/src/main/java/co/com/efalvare/dynamodb/DynamoDBTemplateAdapter.java
@@ -19,7 +19,7 @@ public class DynamoDBTemplateAdapter extends TemplateAdapterOperations<Object /*
         /**
          *  Could be use mapper.mapBuilder if your domain model implement builder pattern
          *  super(repository, mapper, d -> mapper.mapBuilder(d,ObjectModel.ObjectModelBuilder.class).build());
-         *  Or using mapper.map with the class of the object model
+         *  Or using mapper.map with the class of the object model.
          */
         super(connectionFactory, mapper, d -> mapper.map(d, Object.class /*domain model*/), "table_name", "secondary_index" /*index is optional*/);
     }
