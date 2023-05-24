5cf28cfea35979ae77c9a5222869ecd514b162d9
Edwin Felipe Alvarez Rincon <efalvare@bancolombia.com>
2023-05-24 17:42:38 -0500

    [Adiciona] Constantes de la operacion de create-customers

--- lib_commons_comisarias/lib/src/main/java/commons/constants/RabbitOperations.java
+++ lib_commons_comisarias/lib/src/main/java/commons/constants/RabbitOperations.java
@@ -3,6 +3,8 @@ package commons.constants;
 public class RabbitOperations {
     public static final String PROTECTION_MEASURE = "ms_protection_measure";
     public static final String ORCHESTRATOR = "ms_orchestrator";
+    public static final String CUSTOMERS = "ms_customers";
     public static final String CREATE_PROTECTION_MEASURE = "ms_protection_measure.create";
     public static final String GET_STEP = "ms_protection_measure.getStep";
+    public static final String CREATE_CUSTOMERS = "ms_customers.createCustomers";
 }
