6bdba4ef4b66a2ea7f49a7f48e14312ce7ce3934
Edwin Felipe Alvarez Rincon <efalvare@bancolombia.com>
2023-07-16 08:29:36 -0500

    [Ajusta] Modelos de la operacion associate-customer

--- lib_commons_comisarias/lib/src/main/java/commons/rabbit/protectionmeasure/associatecustomer/query/AssociateCustomerDataQuery.java
+++ lib_commons_comisarias/lib/src/main/java/commons/rabbit/protectionmeasure/associatecustomer/query/AssociateCustomerDataQuery.java
@@ -6,11 +6,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder(toBuilder = true)
 public class AssociateCustomerDataQuery {
+    private UUID protectionMeasureId;
     private List<CustomerQuery> customerList;
 }
